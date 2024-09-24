b7579e36b2dbad4997d08a05f9b283da696f4568
P-MBD <p.mobedi@yahoo.com>
2024-09-24 12:57:11 +0330

    StationFragment

--- app/src/main/java/com/example/bustehran/MainActivity.java
+++ app/src/main/java/com/example/bustehran/MainActivity.java
@@ -11,26 +11,17 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    private StationViewModel stationViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        RecyclerView recyclerView = findViewById(R.id.recycler_view);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setHasFixedSize(true);
-
-        StationAdapter adapter = new StationAdapter();
-        recyclerView.setAdapter(adapter);
-
-        stationViewModel = new ViewModelProvider(this).get(StationViewModel.class);
-        stationViewModel.getAllStations().observe(this, new Observer<List<Station>>() {
-            @Override
-            public void onChanged(List<Station> stations) {
-                adapter.setStations(stations);
-            }
-        });
+        // بارگذاری Fragment
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, new StationFragment())
+                    .commit();
+        }
     }
 }
